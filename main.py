8efbaa79550d5f6b093eb427e7af0f6295a52024
Daniel.rb <daniel_sk8_nicolini@hotmail.com>
2023-06-23 12:15:48 -0300

    API com status code 302 finalizada

--- main.py
+++ main.py
@@ -11,20 +11,16 @@ urls = {}
 @app.route('/api/encurtar', methods=['POST'])
 def encurtar():
     url = request.json.get('url')  # Obtém a URL original do corpo da requisição POST
-    if url:
-        # Gere uma chave única para a URL curta (por exemplo, utilizando hash ou algum algoritmo de encurtamento)
-        def generate_short_code(length=6):
-            chars = string.ascii_letters + string.digits
-            code = ''.join(random.choice(chars) for _ in range(length))
-            return code # Algoritmon de Encurtamento
-        
-        # Armazena a URL original e a URL curta no dicionário
-        urls[chave] = url
+    def generate_short_code():
+        if url:
+            chave = generate_short_code()  # Gera uma chave única para a URL curta
+            urls[chave] = url  # Armazena a URL original e a URL curta no dicionário
 
         # Retorna a URL curta gerada
-        return jsonify(url_curta=f'http://localhost:5000/{chave}')
+            return jsonify(url_curta=f'http://localhost:5000/{chave}', code=302)
 
-    return jsonify(erro='URL inválida'), 400  # Retorna uma mensagem de erro caso a URL não seja fornecida
+        return jsonify(erro='URL inválida'), 400  # Retorna uma mensagem de erro caso a URL não seja fornecida
+
 
 @app.route('/<chave>')
 def redirecionar(chave):
@@ -38,27 +34,16 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 #retornar a url encurtada, usando http status code 302
-@app.route("/redirect-external", methods=["GET"])
-def redirect_external():
-    return redirect("https://www.exemplo.com.br/", code=302)
-
-@app.route("/redirect-internal", methods=["GET"])
-def redirect_internal():
-    return redirect("/landing", code=302)
-
-@app.route("/landing", methods=["GET"])
-def landing():
-    return "Internal redirect."
-
-@app.route("/", methods=["GET"])
-def index():
-    return '<a href="/redirect-internal">Internal redirect</a>' \
-           '<br>' \
-           '<a href="/redirect-external">External redirect</a>'
+@app.route('/<chave>')
+def redirecionar(chave):
+    if chave in urls:
+        # Redireciona para a URL original correspondente com o status code 302
+        return redirect(urls[chave], code=302)
+    else:
+        return jsonify(erro='URL curta inválida'), 404  # Retorna uma mensagem de erro caso a chave não seja encontrada
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
-
+    app.run(debug=True)
 
 
 
